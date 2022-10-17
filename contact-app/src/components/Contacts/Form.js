57d5e45ff98cdbde4498f2f0755fd138ed087f60
Eray Ateş <bligzer@outlook.com>
2022-10-17 03:43:19 +0300

    Contact app created. Adding and selection functions implented.

---
+++ contact-app/src/components/Contacts/Form.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { nanoid } from '@reduxjs/toolkit'
+import {useDispatch} from 'react-redux'
+import {addContact,addContacts} from '../../redux/contactSlice'
+
+function Form() {
+  const [name,setName] = useState('')
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!name) return false
+    dispatch(addContact({id:nanoid(),name}))
+    setName('')
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+        <input value={name} onChange={(e) => setName(e.target.value)}/>
+    </form>
+  )
+}
+
+export default Form
